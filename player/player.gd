b97319c0f1cbf5d7cdadb87f916ae64b80ededb9
Philip Wee <philip.andrewweedewang@gmail.com>
2019-12-11 16:04:49 +0800

    Working Movement Code

---
+++ player/player.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+export var speed = 1
+
+onready var joystick_move := $UI/JoystickMove
+
+var vel = Vector2.ZERO
+
+func _move():
+	
+	#----Joystick Movement Code----
+	if joystick_move and joystick_move.is_working:
+		vel += joystick_move.output * speed
+		
+	var collision = move_and_collide(vel)
+	if collision:
+		vel = Vector2.ZERO
+	#----Joystick Movement Code----
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	_move()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
