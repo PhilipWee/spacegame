0ea86a747393394a16e0a1fb659e7feb47110e75
Philip Wee <philip.andrewweedewang@gmail.com>
2019-12-11 17:00:03 +0800

    Shooting Aiming Joystick Working

--- player/player.gd
+++ player/player.gd
@@ -2,26 +2,46 @@ extends KinematicBody2D
 
 export var speed = 1
 
+enum shoot_state {STATE_UNREADY,STATE_READY}
+
 onready var joystick_move := $UI/JoystickMove
+onready var joystick_shoot := $UI/JoystickShoot
+
 
 var vel = Vector2.ZERO
+var current_shoot_state
+var last_shoot_output
 
 func _move():
 	
 	#----Joystick Movement Code----
 	if joystick_move and joystick_move.is_working:
 		vel += joystick_move.output * speed
-	#----Joystick Movement Code----
+	
 	
 	#----Gravity Handling Code----
-	vel += $"../gravityHandler".get_gravity_field(position)
+	if  $"../gravityHandler":
+		vel += $"../gravityHandler".get_gravity_field(position)
 	
 	
 	#----Collision Handling Code----
 	var collision = move_and_collide(vel)
 	if collision:
 		vel = Vector2.ZERO
-	#----Collision Handling Code----
+
+func _shoot():
+	
+	#----Shooting handling Code----
+	if joystick_shoot and joystick_shoot.is_working:
+		current_shoot_state = shoot_state.STATE_READY
+	
+	if current_shoot_state == shoot_state.STATE_READY and not joystick_shoot.is_working:
+		current_shoot_state = shoot_state.STATE_UNREADY
+		print('pew')
+	
+	if joystick_shoot.output == Vector2.ZERO:
+		current_shoot_state = shoot_state.STATE_UNREADY
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -29,6 +49,7 @@ func _ready():
 
 func _physics_process(delta):
 	_move()
+	_shoot()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
